53556a20131c88af1e7b6a315fd8f9e447b53c32
sollwar <sollwarmorgrind@gmail.com>
2022-03-03 20:25:16 +0300

    Задание E сделано

--- src/main/kotlin/dottriangle/MainDotTriangle.kt
+++ src/main/kotlin/dottriangle/MainDotTriangle.kt
@@ -16,6 +16,13 @@ fun dotTriangle(a: Int, b: Int, d: Int): Int {
     val x2 = (d - a) * (d - 0) - (0 - d) * (0 - b)
     val x3 = (0 - a) * (0 - d)
 
+    val aX = sqrt(((a - 0.0) * (a - 0.0)) + ((b - 0.0) * (b - 0.0)))
+    val bX = sqrt(((a - d) * (a - d)) + ((b - 0.0) * (b - 0.0)))
+    val cX = sqrt(((a - 0.0) * (a - 0.0)) + ((b - d) * (b - d)))
+
     if ((x1 >= 0 && x2 >= 0 && x3 >= 0) || (x1 <= 0 && x2 <= 0 && x3 <= 0)) return 0
-    return 1
+
+    return if (aX <= bX && aX <= cX) 1
+    else if (bX <= aX && bX <= cX) 2
+    else 3
 }
