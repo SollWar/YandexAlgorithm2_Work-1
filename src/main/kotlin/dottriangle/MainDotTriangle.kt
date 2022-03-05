71e105850d3d6181ca24a413dde7d69de6d5d17f
Denisov Nikita <sollwarmorgrind@gmail.com>
2022-03-05 21:36:21 +0300

    Немного комментариев

--- src/main/kotlin/dottriangle/MainDotTriangle.kt
+++ src/main/kotlin/dottriangle/MainDotTriangle.kt
@@ -12,15 +12,17 @@ fun main() {
 }
 
 fun dotTriangle(a: Int, b: Int, d: Int): Int {
+    // Проверка входит ли точка - нагло стырено с интернета
     val x1 = 0 - (d - 0) * (0 - b)
     val x2 = (d - a) * (d - 0) - (0 - d) * (0 - b)
     val x3 = (0 - a) * (0 - d)
 
+    if ((x1 >= 0 && x2 >= 0 && x3 >= 0) || (x1 <= 0 && x2 <= 0 && x3 <= 0)) return 0
+
+    // Расстояния от точки до вершин
     val aX = sqrt(((a - 0.0) * (a - 0.0)) + ((b - 0.0) * (b - 0.0)))
     val bX = sqrt(((a - d) * (a - d)) + ((b - 0.0) * (b - 0.0)))
     val cX = sqrt(((a - 0.0) * (a - 0.0)) + ((b - d) * (b - d)))
-
-    if ((x1 >= 0 && x2 >= 0 && x3 >= 0) || (x1 <= 0 && x2 <= 0 && x3 <= 0)) return 0
 
     return if (aX <= bX && aX <= cX) 1
     else if (bX <= aX && bX <= cX) 2
--- src/main/kotlin/metro/MainMetro.kt
+++ src/main/kotlin/metro/MainMetro.kt
@@ -1,13 +1,12 @@
 package metro
 
+
 fun main() {
     val input = readLine()!!.split(" ")
     val n = input[0].toInt()
-    val i = if (input[1].toInt() < input[2].toInt()) input[1].toInt() else input[2].toInt()
-    val j = if (input[1].toInt() > input[2].toInt()) input[1].toInt() else input[2].toInt()
+    val i = if (input[1].toInt() < input[2].toInt()) input[1].toInt() else input[2].toInt() // меняем местами если
+    val j = if (input[1].toInt() > input[2].toInt()) input[1].toInt() else input[2].toInt() // что-бы i > j
     val right = j - i - 1
     val left = (i - 1) + (n - j)
     if (right > left) print(left) else print(right)
-    //println("N: $n| i: $i| j: $j")
-    //println("Left: $left| Right: $right")
 }
